a84bc7193a8315c24da67a16384505b8b6317aea
louwiiii <louisgauthier46@gmail.com>
2024-02-01 00:35:49 +0100

    Fix issues on Firefox

--- app/popup.js
+++ app/popup.js
@@ -1,31 +1,41 @@
 // popup.js
 
+function getBrowserAPI() {
+  if (typeof browser !== 'undefined') {
+      return browser;
+  } else if (typeof chrome !== 'undefined') {
+      return chrome;
+  }
+  throw new Error('No suitable API namespace found');
+}
+const API = getBrowserAPI();
+
 document.addEventListener('DOMContentLoaded', function () {
     var toggleButton = document.getElementById('toggleButton');
   
     // Add click event listener to the button
     toggleButton.addEventListener('click', function () {
-      chrome.storage.sync.get(['extensionDisabled'], function (result) {
+      API.storage.sync.get(['extensionDisabled'], function (result) {
         var extensionDisabled = result.extensionDisabled;
         if (!extensionDisabled) {
           // Disable the extension
-          chrome.storage.sync.set({ extensionDisabled: true }, function () {
+          API.storage.sync.set({ extensionDisabled: true }, function () {
             toggleButton.textContent = 'Enable Extension';
             toggleButton.classList.remove('disabled');
 
              // Send message to content script to update the extension state
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { message: 'extensionDisabled' });
+            API.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                API.tabs.sendMessage(tabs[0].id, { message: 'extensionDisabled' });
             });
           });
         } else {
           // Enable the extension
-          chrome.storage.sync.set({ extensionDisabled: false }, function () {
+          API.storage.sync.set({ extensionDisabled: false }, function () {
             toggleButton.textContent = 'Disable Extension';
             toggleButton.classList.add('disabled');
             // Send message to content script to update the extension state
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { message: 'extensionEnabled' });
+            API.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                API.tabs.sendMessage(tabs[0].id, { message: 'extensionEnabled' });
             });
           });
         }
@@ -34,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
   
 // Get the initial state of the extension and update the button
-chrome.storage.sync.get(['extensionDisabled'], function (result) {
+API.storage.sync.get(['extensionDisabled'], function (result) {
     var extensionDisabled = result.extensionDisabled;
     if (!extensionDisabled) {
     toggleButton.textContent = 'Disable Extension';
--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,7 +1,18 @@
 // content.js
 
-var extensionDisabled = true;
-chrome.storage.sync.get(['extensionDisabled'], function (result) {
+function getBrowserAPI() {
+    if (typeof browser !== 'undefined') {
+        return browser;
+    } else if (typeof chrome !== 'undefined') {
+        return chrome;
+    }
+    throw new Error('No suitable API namespace found');
+}
+
+const API = getBrowserAPI();
+
+var extensionDisabled = false;
+API.storage.sync.get(['extensionDisabled'], function (result) {
     extensionDisabled= result.extensionDisabled;
 });
 
@@ -41,10 +52,8 @@ function convertAdDurationToSeconds(duration) {
 // Function to check for ads periodically
 function checkForAds() {
     if (extensionDisabled) {
-        // console.log('YouTube Ad Skipper: Extension disabled');
         return;
     }
-    // console.log('YouTube Ad Skipper: Checking for ads');
 
     const adPlayer = document.querySelector('.video-ads.ytp-ad-module');
     if (adPlayer) {
@@ -53,7 +62,7 @@ function checkForAds() {
 }
 
 // Listen for messages from the popup script
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+API.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === 'extensionEnabled') {
       // Enable/disable the extension based on the message
       extensionDisabled = false;
