aa0cedb625eb44626dadfa430ca1270690f0d156
louwiiii <louisgauthier46@gmail.com>
2024-01-31 19:34:31 +0100

    First working version of the extension

---
+++ app/scripts/content.js
@@ -0,0 +1,76 @@
+// content.js
+
+var extensionDisabled = true;
+chrome.storage.sync.get(['extensionDisabled'], function (result) {
+    extensionDisabled= result.extensionDisabled;
+});
+
+// Function to be executed when an ad appears
+function handleAd() {
+    if (extensionDisabled) {
+        return;
+    }
+
+    // Set the video time to the end by simulating seeking
+    const videoPlayer = document.querySelector('video.html5-main-video');
+    const adDurationObject = document.querySelector('.ytp-ad-duration-remaining .ytp-ad-text');
+    if (!adDurationObject) {
+        return;
+    }
+    const adDuration = adDurationObject.textContent;
+    const adDurationInSeconds = convertAdDurationToSeconds(adDuration);
+
+    videoPlayer.currentTime = adDurationInSeconds;
+    const skipButton = document.querySelector('.ytp-ad-skip-button-container button'); //.ytp-ad-skip-button-modern
+    if (skipButton) {
+        skipButton.click();
+        console.log('YouTube Ad Skipper: Ad skipped');
+    }
+}
+
+// Function to convert ad duration to seconds
+function convertAdDurationToSeconds(duration) {
+    const parts = duration.split(':');
+    let seconds = 0;
+    for (let i = 0; i < parts.length; i++) {
+        seconds = seconds * 60 + parseInt(parts[i], 10);
+    }
+    return seconds;
+}
+
+// Function to check for ads periodically
+function checkForAds() {
+    if (extensionDisabled) {
+        // console.log('YouTube Ad Skipper: Extension disabled');
+        return;
+    }
+    // console.log('YouTube Ad Skipper: Checking for ads');
+
+    const adPlayer = document.querySelector('.video-ads.ytp-ad-module');
+    if (adPlayer) {
+        handleAd();
+    }
+}
+
+// Listen for messages from the popup script
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === 'extensionEnabled') {
+      // Enable/disable the extension based on the message
+      extensionDisabled = false;
+      sendResponse({ success: true });
+    }
+    if (request.message === 'extensionDisabled') {
+        // Enable/disable the extension based on the message
+        extensionDisabled = true;
+        sendResponse({ success: true });
+    }
+
+  });
+  
+
+// Check for ads when the page is loaded or navigated
+window.addEventListener('load', checkForAds);
+window.addEventListener('spfdone', checkForAds); // For YouTube's SPFx (Single Page Framework)
+
+// Check for ads periodically
+setInterval(checkForAds, 250);
